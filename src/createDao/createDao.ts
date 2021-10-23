dc1a072d2252cfeba26f1ef1f12cb1f0bd762e2a
Doug Kent <github@douglaskent.com>
2021-10-23 21:53:17 +0100

    clean enough UI

--- src/createDao/createDao.ts
+++ src/createDao/createDao.ts
@@ -1,17 +1,30 @@
+import { TelegramDaoService } from "./../services/TelegramDaoService";
 import { Utils } from "services/utils";
-import { computedFrom } from "aurelia-framework";
-import { Address } from "services/EthereumService";
+import { autoinject } from "aurelia-framework";
+import { Address, EthereumService } from "services/EthereumService";
+import { RouteConfig } from "aurelia-router";
 import "./createDao.scss";
 
+@autoinject
 export class createDao {
   initialOwners: Array<Address> = new Array<string>();
   newOwner: Address;
-  // channnelName: string;
-  // telegramHandle: string;
+  threshold: number;
+  chatId: string;
+  chatTitle: string;
 
-  @computedFrom()
+  constructor(
+    private ethereumService: EthereumService,
+    private telegramDaoService: TelegramDaoService,
+  ) {}
+
+  activate(params: unknown, routeConfig: RouteConfig): void {
+    console.dir(params);
+  }
+
+  // @computedFrom()
   get isValid(): boolean {
-    return this.initialOwners.length > 0; //  !!(this.channnelName?.length && this.telegramHandle?.length && this.telegramHandle.startsWith("@"));
+    return (this.initialOwners.length > 0) && (this.threshold < (this.initialOwners.length + 1)); //  !!(this.channnelName?.length && this.telegramHandle?.length && this.telegramHandle.startsWith("@"));
   }
 
   deleteOwner(ndx: number): void {
@@ -25,9 +38,15 @@ export class createDao {
     }
   }
 
-  createDAO(): void {
-    if (this.isValid) {
+  connect(): void {
+    this.ethereumService.ensureConnected();
+  }
 
+  createDAO(): void {
+    if (this.isValid && this.ethereumService.defaultAccountAddress) {
+      // this.telegramDaoService.deployDao({
+
+      // });
     }
   }
 }
